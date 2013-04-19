148e4f04ba8ea53f72a0366a8ff1fb0bd4ed4375
Adelbert Chang <adelbertc@gmail.com>
2013-04-18 23:54:13 -0700

    Added merge method to MerkleTree
    merge takes two MerkleTrees, computes the parent
    node for the roots of both trees, and returns it.
    
    Return type for merge as well as the helper function
    inside makeTree changed from Tree[A] => Node[A] since
    creating a parent implies it is a Node, not a Leaf.

--- src/main/scala/merkle/MerkleTree.scala
+++ src/main/scala/merkle/MerkleTree.scala
@@ -15,32 +15,6 @@ object MerkleTree {
     override val hash: Vector[Byte] = Vector.empty[Byte]
   }
 
-  private def calculateRequiredLevel(numberOfDataBlocks: Int): Int = {
-    def log2(x: Double): Double = math.log(x) / math.log(2)
-
-    math.ceil(log2(numberOfDataBlocks)).toInt
-  }
-
-  private def dataHashPairToLeaf[A](dataHashPair: (Vector[Byte], A)): Leaf[A] =
-    Leaf(dataHashPair._1, Some(dataHashPair._2))
-
-  @tailrec
-  private def makeTree[A](trees: Seq[Tree[A]], hashFunction: CryptographicHash): Tree[A] = {
-    def createParents[A](treePair: Seq[Tree[A]]): Tree[A] = {
-      val child1 +: child2 +: _ = treePair
-      val newHash = hashFunction.hash((child1.hash ++ child2.hash))
-      Node(newHash, child1, child2)
-    }
-
-    if (trees.size == 0) {
-      EmptyLeaf
-    } else if (trees.size == 1) {
-      trees.head
-    } else {
-      makeTree(trees.grouped(2).map(createParents).toSeq, hashFunction)
-    }
-  }
-
   def create[A](dataBlocks: Seq[A])(implicit hashFunction: CryptographicHash = SHA1Hash): Tree[A] = {
     val level = calculateRequiredLevel(dataBlocks.size)
 
@@ -53,5 +27,35 @@ object MerkleTree {
     val leaves = hashedDataBlocks.zip(dataBlocks).map(dataHashPairToLeaf) ++ padding
 
     makeTree(leaves, hashFunction)
+  }
+
+  def merge[A](leftChild: Tree[A], rightChild: Tree[A])(implicit hashFunction: CryptographicHash = SHA1Hash): Node[A] = {
+    val newHash = hashFunction.hash((leftChild.hash ++ rightChild.hash))
+    Node(newHash, leftChild, rightChild)
+  }
+
+  private def calculateRequiredLevel(numberOfDataBlocks: Int): Int = {
+    def log2(x: Double): Double = math.log(x) / math.log(2)
+
+    math.ceil(log2(numberOfDataBlocks)).toInt
+  }
+
+  private def dataHashPairToLeaf[A](dataHashPair: (Vector[Byte], A)): Leaf[A] =
+    Leaf(dataHashPair._1, Some(dataHashPair._2))
+
+  @tailrec
+  private def makeTree[A](trees: Seq[Tree[A]], hashFunction: CryptographicHash): Tree[A] = {
+    def createParents[A](treePair: Seq[Tree[A]]): Node[A] = {
+      val leftChild +: rightChild +: _ = treePair
+      merge(leftChild, rightChild)(hashFunction)
+    }
+
+    if (trees.size == 0) {
+      EmptyLeaf
+    } else if (trees.size == 1) {
+      trees.head
+    } else {
+      makeTree(trees.grouped(2).map(createParents).toSeq, hashFunction)
+    }
   }
 }
